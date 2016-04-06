c8af2ad7f4a5e96e3073d283a532c4dc339b674e
dhorhehe <thorg3@gmail.com>
2016-04-06 14:25:45 +0200

    Worked on playerbehaviour

---
+++ OneClickGame/Assets/Thor/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    private bool move;
+
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void Movement ()
+    {
+        while (move)
+        {
+           
+        }
+    }
+}
