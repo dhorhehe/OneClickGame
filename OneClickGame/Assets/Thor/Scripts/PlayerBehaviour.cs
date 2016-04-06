50564da3077079bc200f22e4f32b912266b2afca
dhorhehe <thorg3@gmail.com>
2016-04-06 14:54:58 +0200

    Movevment Start

--- OneClickGame/Assets/Thor/Scripts/PlayerBehaviour.cs
+++ OneClickGame/Assets/Thor/Scripts/PlayerBehaviour.cs
@@ -4,27 +4,39 @@ using System.Collections;
 public class PlayerBehaviour : MonoBehaviour
 {
 
-    private bool move;
+    private int move;
 
-    
+    private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start ()
     {
-        
-	}
+        rb = GetComponent<Rigidbody2D>();
+	    move = 0;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    
+        Movement();
 	}
 
     void Movement ()
     {
-        while (move)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-           
+            move += 1;
         }
+
+        if (move%2==0)
+        {
+           rb.AddForce(new Vector2(0, 0.1f) *Time.deltaTime);
+        }
+        if (move%2==1)
+        {
+            rb.AddForce(new Vector2(0, -0.1f) *Time.deltaTime);
+        }
+
+
     }
 }
