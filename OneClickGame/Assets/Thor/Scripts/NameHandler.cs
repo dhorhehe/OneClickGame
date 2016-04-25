e4d10b9dfa863f3dc3e2d777a7b7b082606884f4
dhorhehe <thorg3@gmail.com>
2016-04-25 12:33:29 +0200

    Name works

---
+++ OneClickGame/Assets/Thor/Scripts/NameHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NameHandler : MonoBehaviour
+{
+
+    private string newName;
+    public static string currentName;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    currentName = PlayerPrefs.GetString("currentName");
+	    newName = PlayerPrefs.GetString("NewName");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Debug.Log(currentName);
+
+	    newName = GetComponentInChildren<InputField>().text;
+    }
+
+    public void NameHandlerFunction()
+    {
+        if (currentName == null || currentName != newName)
+        {
+            PlayerPrefs.SetString("currentName", newName);
+            //PlayerPrefs.SetString("newName",currentName);
+            PlayerPrefs.Save();
+        }
+        Debug.Log("Gay");
+    }
+}
